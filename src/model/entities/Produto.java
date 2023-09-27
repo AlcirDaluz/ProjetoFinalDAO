d4c83c4db5b8bebeb16e777986a7de25e55b8b2a
Alcir Daluz <alcirdaluz@protonmail.com>
2023-09-27 17:58:04 -0300

    Criação classe Produto e Usuario

--- src/model/entities/Produto.java
+++ src/model/entities/Produto.java
@@ -1,4 +1,42 @@
 package model.entities;
 
+import java.util.Objects;
+
 public class Produto {
+
+    private String nome;
+
+    public Produto() {
+
+    }
+
+    public Produto(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(nome, produto.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "nome='" + nome + '\'' + '}';
+    }
 }
--- src/model/entities/Usuario.java
+++ src/model/entities/Usuario.java
@@ -1,4 +1,82 @@
 package model.entities;
 
+import java.util.Objects;
+
 public class Usuario {
+
+    private String nome;
+    private String sobrenome;
+    private String login;
+    private String senha;
+    private String confirmacaoSenha;
+
+    public Usuario() {
+
+    }
+
+    public Usuario(String nome, String sobrenome, String login, String senha, String confirmacaoSenha) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.login = login;
+        this.senha = senha;
+        this.confirmacaoSenha = confirmacaoSenha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getConfirmacaoSenha() {
+        return confirmacaoSenha;
+    }
+
+    public void setConfirmacaoSenha(String confirmacaoSenha) {
+        this.confirmacaoSenha = confirmacaoSenha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(nome, usuario.nome) && Objects.equals(sobrenome, usuario.sobrenome) && Objects.equals(login, usuario.login) && Objects.equals(senha, usuario.senha) && Objects.equals(confirmacaoSenha, usuario.confirmacaoSenha);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, sobrenome, login, senha, confirmacaoSenha);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nome='" + nome + '\'' + ", sobrenome='" + sobrenome + '\'' + ", login='" + login + '\'' + ", senha='" + senha + '\'' + ", confirmacaoSenha='" + confirmacaoSenha + '\'' + '}';
+    }
 }
