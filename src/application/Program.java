be53fc7ca55b279101d40caa75eefc823f91e86d
Alcir Daluz <alcirdaluz@protonmail.com>
2023-10-03 18:58:25 -0300

    formatação

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
-import model.dao.UsuarioDao;
 import model.dao.DaoFactory;
+import model.dao.UsuarioDao;
 
 public class Program {
 
     public static void main(String[] args) {
-        
+
         UsuarioDao usuarioDao = DaoFactory.createUsuarioDao();
 
         usuarioDao.menu();
